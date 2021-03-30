7836a392b770cb68ed0a333fdb545c836f480341
Sitare Arslanturk <sitare.arslanturk@ykteknoloji.com.tr>
2021-03-31 02:26:08 +0300

    Functions added

--- app/src/main/java/com/sitare/guessgame/MainActivity.kt
+++ app/src/main/java/com/sitare/guessgame/MainActivity.kt
@@ -2,10 +2,51 @@ package com.sitare.guessgame
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.core.view.isGone
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    var score = 100
+    var randomNumber = (1..100).random()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        checkButton.setOnClickListener {
+            val guess = guessTextInputView.text
+
+            if (guess.toString().isEmpty()){
+                guessTextInputView.error = "Please enter a number"
+            } else {
+                when {
+                    guess.toString().toInt() > randomNumber  -> {
+                        updateScore()
+                        updateTextView("Please enter a smaller number")
+                    }
+                    guess.toString().toInt() < randomNumber  -> {
+                        updateScore()
+                        updateTextView("Please enter a bigger number")
+                    }
+                    else ->  {
+                        updateTextView("CONGRATS! ${getString(R.string.score)} $score")
+                        scoreTextView.isGone = true
+                        guessTextInputView.isEnabled = false
+                        checkButton.isEnabled = false
+                    }
+                }
+            }
+        }
     }
+
+    fun updateScore(){
+        score -= 1
+        scoreTextView.text = "${getString(R.string.score)} $score"
+    }
+
+    fun updateTextView(text:String){
+        statusTextView.text = "$text"
+    }
+
+
 }
